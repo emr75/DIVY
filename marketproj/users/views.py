347ed03482338f879fd519ce1ce871dcf760aaee
gpr2033 <gpr2033@nyu.edu>
2025-10-07 19:10:01 -0400

    add user CRUD apis

--- marketproj/users/views.py
+++ marketproj/users/views.py
@@ -1,16 +1,55 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .utils import UserSerializer
+from .models import User
 
-# Create your views here.
-
+# CREATE user
+@api_view(['POST'])
 def create_user(request):
-    return HttpResponse("User created successfully")
+    user = UserSerializer(data=request.data)
 
+    #TODO: Add server-side validation, make sure its reflected on front-end
+    # if data.is_valid():
+    user.save()
+
+    return Response(user.data, status=status.HTTP_201_CREATED)
+
+# READ user
+@api_view(['GET'])
 def get_user(request):
-    return HttpResponse("User") 
+    try:
+        user = User.objects.get(id=request.GET.get('id'))
+    except User.DoesNotExist:
+        # User not found
+        return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    data = UserSerializer(user)
+    return Response(data.data, status=status.HTTP_200_OK)
 
+# UPDATE user
+@api_view(['PATCH'])
 def update_user(request):
-    return HttpResponse("User updated successfully")
+    try:
+        user = User.objects.get(id=request.data.get('id'))
+    except User.DoesNotExist:
+        # User not found
+        return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    #TODO: Add server-side validation, make sure its reflected on front-end
+    # if data.is_valid():
+    user.save()
+    return Response(user.data, status=status.HTTP_200_OK)
 
+# DELETE user
+@api_view(['DELETE'])
 def delete_user(request):
-    return HttpResponse("User deleted successfully")
+    try:
+        user = User.objects.get(id=request.data.get('id'))
+    except User.DoesNotExist:
+        # User not found
+        return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    user.delete()
+    return Response({'message': 'User deleted successfully'}, status=status.HTTP_200_OK)
