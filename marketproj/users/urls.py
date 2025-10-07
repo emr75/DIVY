79e8653916578b002895d4adf10dc52f250bc58b
gpr2033 <gpr2033@nyu.edu>
2025-10-07 18:34:04 -0400

    added user model

--- marketproj/users/urls.py
+++ marketproj/users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("create", views.index, name="create_user"),
+    path("create", views.create_user, name="create_user"),
     path("get", views.get_user, name="get_user"),
     path("update", views.update_user, name="update_user"),
     path("delete", views.delete_user, name="delete_user"),
