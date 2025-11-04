b5358343d6bb6946603cee77f58e01fe812d82b5
likhith2366 <g.likhithvardhan@gmail.com>
2025-11-04 00:13:21 -0500

    test succesful i formatted using black and re pushing it

--- marketproj/marketproj/urls.py
+++ marketproj/marketproj/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from   django.contrib import     admin
-from django.urls import path,include
+from django.contrib import admin
+from django.urls import path, include
 
 
-urlpatterns=[ path( "users/",include("users.urls")),path(  "auth/",  include( "userauth.urls" )),path("admin/",  admin.site.urls ), ]
+urlpatterns = [
+    path("users/", include("users.urls")),
+    path("auth/", include("userauth.urls")),
+    path("admin/", admin.site.urls),
+]
