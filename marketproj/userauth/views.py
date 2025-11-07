a8dd2e52fe7e60f0e23c43b389fdf6b1321c66f1
Vincent Hepola <vuhepola1227@gmail.com>
2025-11-07 18:13:34 -0500

    applied python/black formatting to /markerproj

--- marketproj/userauth/views.py
+++ marketproj/userauth/views.py
@@ -9,26 +9,36 @@ from users.models import User
 
 from .jwtutils import generate_jwt
 
-# LOGIN user
-@api_view(['POST'])
-def login(request):
-    username = request.data.get('username')
-    password = request.data.get('password')
 
-    #Invalid username or password
+# LOGIN user
+@api_view(["POST"])
+def login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+
+    # Invalid username or password
     try:
         user = User.objects.get(username=username)
     except User.DoesNotExist:
-        return Response({"error": "Invalid username or password"}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(
+            {"error": "Invalid username or password"},
+            status=status.HTTP_401_UNAUTHORIZED,
+        )
 
     if check_password(password, user.password):
         # Successful login
         token = generate_jwt(user)
 
-        return Response({
-            "message": "Login successful",
-            "token": token,
-        }, status=status.HTTP_200_OK)
+        return Response(
+            {
+                "message": "Login successful",
+                "token": token,
+            },
+            status=status.HTTP_200_OK,
+        )
     else:
         # Invalid password
-        return Response({"error": "Invalid username or password"}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(
+            {"error": "Invalid username or password"},
+            status=status.HTTP_401_UNAUTHORIZED,
+        )
